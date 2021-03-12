3045b7a15d7cc614b5e827946b1212da4ef5c960
Sami Kojo-Fry <sami.kojo@tuni.fi>
2021-03-12 11:34:55 +0200

    Harjoitusten esimerkkiratkaisut

--- Exercises/Exercises_class.py
+++ Exercises/Exercises_class.py
@@ -11,6 +11,22 @@
 # Mieti mitä jäsenmuuttujia luokka tarvitsee ominaisuuksien toteuttamiseksi
 # ja määritä ne.
 
+from math import pi
+
+class Shape:
+  def area(self):
+    return None
+
+class Circle(Shape):
+  def __init__(self, r):
+    self.r = r
+
+  def area(self):
+    return pi * self.r**2
+
+  def circumference(self):
+    return 2 * pi * self.r
+
 # Harjoitus 2
 # Määritä luokka Point. Point kuvaa pistettä 2-ulotteisessa avaruudessa.
 # Mieti mitä jäsenmuuttujia kyseinen luokka tarvitsee ja toteuta ne.
@@ -23,3 +39,32 @@
 # - area: palauttaa nelikulmion pinta-alan. Ylikirjoitetaan Shape-luokan toteutus.
 # - width: palauttaa nelikulmion leveyden.
 # - height: palauttaa nelikulmion korkeuden.
+
+class Point:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+class Rectangle(Shape):
+  def __init__(self, bottomLeft, topRight):
+    self.bottomLeft = bottomLeft
+    self.topRight = topRight
+
+  def width(self):
+    return self.topRight.x - self.bottomLeft.x
+
+  def height(self):
+    return self.topRight.y - self.bottomLeft.y
+
+  def area(self):
+    return self.width() * self.height()
+
+
+bottomLeft = Point(0, 0)
+topRight = Point(5, 10)
+
+rectangle = Rectangle(bottomLeft, topRight)
+
+print("Leveys", rectangle.width())
+print("Korkeus", rectangle.height())
+print("Pinta-ala", rectangle.area())
